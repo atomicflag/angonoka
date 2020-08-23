0965f68e16116b57fb48041fdde70d3b4d71f805
Andrew <dronx.wow@gmail.com>
2020-08-23 03:31:02 +0300

    WIP: Minor refactoring

--- src/py/module.cpp
+++ src/py/module.cpp
@@ -34,10 +34,9 @@ using viewv = ranges::span<viewi>;
 using veci = std::vector<Int>;
 using RandomEngine = pcg32;
 
-struct Generator {
-    RandomEngine g{pcg_extras::seed_seq_from<std::random_device>{}};
-    boost::random::uniform_01<float> r;
+class RandomUtils {
 
+public:
     // void get_random(veci& v) noexcept
     // {
     //     const auto task_count = v.size();
@@ -46,28 +45,88 @@ struct Generator {
     //     }
     // }
 
-    inline gsl::index random_index(gsl::index max) noexcept;
-
-    inline std::int_fast16_t
-    pick_random(ranges::span<int_fast16_t> rng) noexcept;
-
     inline void get_neighbor(veci& v) noexcept;
+
+    float get_uniform() noexcept { return r(g); }
+
+private:
+    RandomEngine g{pcg_extras::seed_seq_from<std::random_device>{}};
+    // RandomEngine g{0};
+    boost::random::uniform_01<float> r;
+
+    gsl::index random_index(gsl::index max) noexcept
+    {
+        return static_cast<gsl::index>(r(g) * max);
+    }
+
+    std::int_fast16_t
+    pick_random(ranges::span<std::int_fast16_t> rng) noexcept
+    {
+        return rng[random_index(rng.size())];
+    }
 };
+
+static_assert(std::is_nothrow_destructible_v<RandomUtils>);
+static_assert(std::is_default_constructible_v<RandomUtils>);
+static_assert(std::is_nothrow_copy_constructible_v<RandomUtils>);
+static_assert(std::is_nothrow_copy_assignable_v<RandomUtils>);
+static_assert(std::is_nothrow_move_constructible_v<RandomUtils>);
+static_assert(std::is_nothrow_move_assignable_v<RandomUtils>);
+
+int total_size(const std::vector<veci>& v)
+{
+    return ranges::accumulate(v, 0, [](auto acc, auto&& i) {
+        return acc + i.size();
+    });
+}
+
+class TaskAgents {
+public:
+    TaskAgents() = default;
+    TaskAgents(const std::vector<veci>& data)
+        : int_data{std::make_unique<Int[]>(total_size(data))}
+        , spans{std::make_unique<viewi[]>(data.size())}
+        , task_agents{spans.get(), static_cast<long>(data.size())}
+    {
+        Int* int_data_ptr = int_data.get();
+        viewi* spans_ptr = spans.get();
+        for (auto&& v : data) {
+            *spans_ptr++
+                = {int_data_ptr, static_cast<long>(v.size())};
+            int_data_ptr = ranges::copy(v, int_data_ptr).out;
+        }
+    }
+
+    decltype(auto) operator[](gsl::index i) const noexcept
+    {
+        return task_agents[i];
+    }
+
+private:
+    std::unique_ptr<Int[]> int_data;
+    std::unique_ptr<viewi[]> spans;
+    viewv task_agents;
+};
+
+static_assert(std::is_nothrow_destructible_v<TaskAgents>);
+static_assert(std::is_nothrow_default_constructible_v<TaskAgents>);
+static_assert(!std::is_copy_constructible_v<TaskAgents>);
+static_assert(!std::is_copy_assignable_v<TaskAgents>);
+static_assert(std::is_nothrow_move_constructible_v<TaskAgents>);
+static_assert(std::is_nothrow_move_assignable_v<TaskAgents>);
 
 enum class AgentIndex : gsl::index {};
 enum class TaskIndex : gsl::index {};
 
 struct Data {
     std::unique_ptr<float[]> float_data;
-    std::unique_ptr<Int[]> int_data;
-    std::unique_ptr<viewi[]> spans;
 
-    viewv agent_tasks;
-    viewv task_agents;
+    TaskAgents task_agents;
 
     gsl::index task_count;
     gsl::index agent_count;
 
+    // TODO: Refactor into TaskDurations
     gsl::index get_task_duration_index(
         AgentIndex agent,
         TaskIndex task) const noexcept
@@ -76,8 +135,10 @@ struct Data {
             + static_cast<gsl::index>(agent);
     }
 
-    void
-    set_task_duration_for(AgentIndex agent, TaskIndex task, float value)
+    void set_task_duration_for(
+        AgentIndex agent,
+        TaskIndex task,
+        float value)
     {
         float_data[get_task_duration_index(agent, task)] = value;
     }
@@ -88,23 +149,12 @@ struct Data {
         return float_data[get_task_duration_index(agent, task)];
     }
 
-    std::vector<Generator> gens;
+    std::vector<RandomUtils> random_utils;
 };
 
 static Data data;
 
-inline gsl::index Generator::random_index(gsl::index max) noexcept
-{
-    return static_cast<gsl::index>(r(g) * max);
-}
-
-inline std::int_fast16_t
-Generator::pick_random(ranges::span<int_fast16_t> rng) noexcept
-{
-    return rng[random_index(rng.size())];
-}
-
-inline void Generator::get_neighbor(veci& __restrict v) noexcept
+inline void RandomUtils::get_neighbor(veci& __restrict v) noexcept
 {
     const auto task_idx = random_index(v.size());
     v[task_idx] = pick_random(data.task_agents[task_idx]);
@@ -116,7 +166,7 @@ viewf insert_data(const vecf& in, float* data)
     return {data, static_cast<long>(in.size())};
 }
 
-auto flatten(const std::vector<veci>& in, Int* data, viewi* spans)
+auto to_spans(const std::vector<veci>& in, Int* data, viewi* spans)
 {
     for (auto&& v : in) {
         *spans = {data, static_cast<long>(v.size())};
@@ -124,13 +174,6 @@ auto flatten(const std::vector<veci>& in, Int* data, viewi* spans)
         data = ranges::copy(v, data).out;
     }
     return std::make_tuple(data, spans);
-}
-
-int total_size(const std::vector<veci>& v)
-{
-    return ranges::accumulate(v, 0, [](auto acc, auto&& i) {
-        return acc + i.size();
-    });
 }
 
 void init(
@@ -141,42 +184,29 @@ void init(
     const auto task_duration_count
         = task_durations.size() * agent_performances.size();
     data.float_data = std::make_unique<float[]>(task_duration_count);
-    data.int_data = std::make_unique<Int[]>(total_size(task_agents));
-    data.spans = std::make_unique<viewi[]>(
-        task_durations.size() + agent_performances.size());
     data.task_count = task_durations.size();
     data.agent_count = agent_performances.size();
+    data.task_agents = {task_agents};
 
-    for (gsl::index i{0}; i < task_durations.size(); ++i) {
-        for (gsl::index j{0}; j < agent_performances.size(); ++j) {
-            data.set_task_duration_for(
-                AgentIndex{j},
-                TaskIndex{i},
-                task_durations[i] / agent_performances[j]);
-        }
-    }
-
-    flatten(task_agents, data.int_data.get(), data.spans.get());
-
-    data.task_agents
-        = {data.spans.get(), static_cast<long>(task_agents.size())};
-    data.gens.resize(omp_get_max_threads());
+    // data.random_utils.resize(0);
+    data.random_utils.resize(omp_get_max_threads());
 }
 
-float stun(float lowest_e, float current_e) noexcept
+float stun(float lowest_e, float current_e, float alpha) noexcept
 {
-    constexpr auto alpha = .5f;
+    // TODO: Binary search the hyperparameter
+    // constexpr auto alpha = 1.2f;
     return 1.f - std::exp(-alpha * (current_e - lowest_e));
 }
 
-enum class AgentCount : gsl::index {};
-
 class SchedulingUtils {
 public:
-    SchedulingUtils(
-        AgentCount agent_count,
-        gsl::not_null<Data*> data) noexcept
-        : makespan_buffer(static_cast<gsl::index>(agent_count))
+    SchedulingUtils(gsl::not_null<Data*> data) noexcept
+        : makespan_buffer_data(
+            std::make_unique<float[]>(data->agent_count))
+        , makespan_buffer(
+              makespan_buffer_data.get(),
+              data->agent_count)
         , data{data}
     {
     }
@@ -195,11 +225,13 @@ public:
     }
 
 private:
-    vecf makespan_buffer;
+    std::unique_ptr<float[]> makespan_buffer_data;
+    ranges::span<float> makespan_buffer;
     gsl::not_null<Data*> data;
 };
 
-std::tuple<veci, float> epoch(veci&& best_state, float beta)
+std::tuple<veci, float>
+epoch(veci&& best_state, float beta, float alpha, float beta_scale)
 {
     // CALLGRIND_START_INSTRUMENTATION;
     // CALLGRIND_TOGGLE_COLLECT;
@@ -214,39 +246,41 @@ std::tuple<veci, float> epoch(veci&& best_state, float beta)
         veci current_state = best_state;
         veci target_state(current_state.size());
 
-        SchedulingUtils utils{AgentCount{data.agent_count}, &data};
+        SchedulingUtils utils{&data};
         float current_e = utils.makespan_of(current_state);
         float lowest_e = current_e;
 
-        float current_s = stun(lowest_e, current_e);
+        float current_s = stun(lowest_e, current_e, alpha);
         float average_stun = 0.f;
         count_t stun_count = 0u;
         float last_average_stun;
 
-        auto& __restrict gen = data.gens[omp_get_thread_num()];
-        constexpr count_t max_iterations = 10'000'000u;
+        auto& __restrict random_utils
+            = data.random_utils[omp_get_thread_num()];
+        // constexpr count_t max_iterations = 10'000'000u;
+        constexpr count_t max_iterations = 1'000'000u;
         for (count_t i{0}; i < max_iterations; ++i) {
             // target_state = current_state;
             ranges::copy(current_state, target_state.begin());
-            gen.get_neighbor(target_state);
+            random_utils.get_neighbor(target_state);
             const auto target_e = utils.makespan_of(target_state);
             if (__builtin_expect(target_e < lowest_e, 0)) {
                 lowest_e = target_e;
                 // best_state = target_state;
                 ranges::copy(target_state, best_state.begin());
-                current_s = stun(lowest_e, current_e);
-                fmt::print("{}\n", lowest_e);
+                current_s = stun(lowest_e, current_e, alpha);
+                // fmt::print("{}\n", lowest_e);
             }
             if (__builtin_expect(target_e < current_e, 0)) {
                 current_state.swap(target_state);
                 current_e = target_e;
                 continue;
             }
-            const float target_s = stun(lowest_e, target_e);
+            const float target_s = stun(lowest_e, target_e, alpha);
             const auto pr = std::min(
                 1.f,
                 std::exp(-beta * (target_s - current_s)));
-            if (pr >= gen.r(gen.g)) {
+            if (pr >= random_utils.get_uniform()) {
                 current_state.swap(target_state);
                 current_e = target_e;
                 current_s = target_s;
@@ -254,18 +288,18 @@ std::tuple<veci, float> epoch(veci&& best_state, float beta)
                 ++stun_count;
             }
             // TODO: Move into Beta (temp) scheduler?
-            constexpr auto average_stun_window = 100'000u;
+            constexpr auto average_stun_window = max_iterations / 100;
             if (stun_count >= average_stun_window) {
                 average_stun /= stun_count;
                 last_average_stun = average_stun;
                 const auto diff = average_stun - 0.03f;
                 const auto t
                     = 1.f - static_cast<float>(i) / max_iterations;
-                beta *= 1.f + diff * .1f * t * t;
+                beta *= 1.f + diff * beta_scale * t * t;
                 stun_count = 0u;
             }
         }
-        fmt::print("average_stun: {}\n", last_average_stun);
+        // fmt::print("average_stun: {}\n", last_average_stun);
 #pragma omp critical
         {
             if (global_state.empty() || lowest_e < global_e) {
