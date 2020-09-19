36f06278b9f41597b553a6dbf47cb0b50bd5ac9c
Andrew <dronx.wow@gmail.com>
2020-09-19 03:09:40 +0300

    Add TaskDurationCache tests

--- test/stun/task_duration_cache.cpp
+++ test/stun/task_duration_cache.cpp
@@ -1,7 +1,6 @@
 #include "stun/task_duration_cache.h"
 #include <catch2/catch.hpp>
-
-// TODO: Add more tests
+#include <vector>
 
 TEST_CASE("TaskDurationCache type traits")
 {
@@ -19,5 +18,16 @@ TEST_CASE("TaskDurationCache type traits")
 
 TEST_CASE("TaskDurationCache values")
 {
-    // pass
+    using angonoka::stun::AgentIndex;
+    using angonoka::stun::TaskDurationCache;
+    using angonoka::stun::TaskIndex;
+
+    const std::vector<float> data{1.f, 2.f, 3.f};
+
+    const TaskDurationCache cache{data, data};
+
+    REQUIRE(cache.get(AgentIndex{0}, TaskIndex{0}) == 1.f);
+    REQUIRE(cache.get(AgentIndex{1}, TaskIndex{1}) == 1.f);
+    REQUIRE(cache.get(AgentIndex{2}, TaskIndex{2}) == 1.f);
+    REQUIRE(cache.get(AgentIndex{0}, TaskIndex{2}) == 3.f);
 }
