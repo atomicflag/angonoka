1f05e0c083b5f5b01bfed8379534af1ea6ef8896
Andrew <dronx.wow@gmail.com>
2020-10-27 04:07:21 +0300

    Implement benchmark [skip ci]

--- benchmark/stochastic_tunneling.cpp
+++ benchmark/stochastic_tunneling.cpp
@@ -10,9 +10,12 @@
 #include <range/v3/view/chunk.hpp>
 #include <range/v3/view/cycle.hpp>
 #include <range/v3/view/iota.hpp>
+#include <range/v3/view/linear_distribute.hpp>
 #include <range/v3/view/take.hpp>
 #include <thread>
 #include <vector>
+
+#include <fmt/printf.h>
 
 namespace {
 using namespace angonoka::stun;
@@ -20,23 +23,27 @@ using ranges::to;
 using ranges::views::chunk;
 using ranges::views::cycle;
 using ranges::views::iota;
+using ranges::views::linear_distribute;
 using ranges::views::take;
 
 struct STUNFixture : celero::TestFixture {
-    static constexpr auto agent_count = 3;
+    static constexpr auto agent_count = 10;
     static constexpr auto task_count = agent_count * 5;
 
-    std::vector<int16> agent_indices = iota(0, 2) | cycle
+    std::vector<int16> agent_indices = iota(0, agent_count) | cycle
         | take(task_count * agent_count) | to<std::vector<int16>>();
     std::vector<span<const int16>> spans = agent_indices
         | chunk(agent_count) | to<std::vector<span<const int16>>>();
     // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-    std::vector<float> task_durations{.1F, .3F, .4F, .5F, .7F};
-    std::vector<float> agent_performances = std::vector(agent_count, 1.F);
+    std::vector<float> task_durations
+        = linear_distribute(.1F / 2.6F, .9F / 2.6F, 5) | cycle
+        | take(task_count) | to<std::vector<float>>();
+    std::vector<float> agent_performances
+        = std::vector(agent_count, 1.F);
 
     TaskAgents task_agents{spans};
-    static constexpr auto random_engine_seed = 123;
-    RandomUtils random_utils{&task_agents, random_engine_seed};
+    // static constexpr auto random_engine_seed = 123;
+    RandomUtils random_utils{&task_agents};
     TaskDurationCache task_duration{
         task_durations,
         agent_performances};
@@ -47,5 +54,21 @@ struct STUNFixture : celero::TestFixture {
 // NOLINTNEXTLINE(readability-redundant-member-init)
 BASELINE_F(DemoSimple, Baseline, STUNFixture, 0, 0)
 {
-    std::this_thread::sleep_for(std::chrono::nanoseconds{1});
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-braces"
+    const std::vector<int16> best_state(task_count, 0);
+    const auto result = stochastic_tunneling(
+        random_utils,
+        makespan,
+        best_state,
+        Gamma{.5F},
+        Beta{1.F},
+        BetaScale{.3F});
+    celero::DoNotOptimizeAway(result);
+    // TODO: percentile
+    // fmt::print("{}\n", result.energy);
+    // for(const auto& v : result.state)
+    //     fmt::print("{} ", v);
+    // fmt::print("\n");
+#pragma clang diagnostic pop
 }
