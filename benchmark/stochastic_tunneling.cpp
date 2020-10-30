6a7cc2824a287abacb5bdbc07914e350aa67c1b0
Andrew <dronx.wow@gmail.com>
2020-10-31 02:02:30 +0300

    Update benchmark [skip ci]

--- benchmark/stochastic_tunneling.cpp
+++ benchmark/stochastic_tunneling.cpp
@@ -27,11 +27,17 @@ using ranges::views::linear_distribute;
 using ranges::views::take;
 
 struct STUNState {
+    STUNState(gsl::index agent_count, gsl::index tasks_per_agent)
+        : agent_count{agent_count}
+        , tasks_per_agent{tasks_per_agent}
+    {
+    }
     gsl::index agent_count = 5;
     gsl::index tasks_per_agent = 5;
     gsl::index task_count = agent_count * tasks_per_agent;
 
-    std::vector<int16> agent_indices = iota(0, static_cast<int>(agent_count)) | cycle
+    std::vector<int16> agent_indices
+        = iota(0, static_cast<int>(agent_count)) | cycle
         | take(task_count * agent_count) | to<std::vector<int16>>();
     std::vector<span<const int16>> spans = agent_indices
         | chunk(agent_count) | to<std::vector<span<const int16>>>();
@@ -57,44 +63,62 @@ struct STUNState {
     std::vector<int16> best_state = std::vector<int16>(task_count, 0);
 };
 
+class LowestEnergyUDM
+    : public celero::UserDefinedMeasurementTemplate<float> {
+    [[nodiscard]] std::string getName() const override
+    {
+        return "Lowest Energy";
+    }
+};
+
 struct STUNFixture : celero::TestFixture {
 
     [[nodiscard]] std::vector<celero::TestFixture::ExperimentValue>
     getExperimentValues() const override
     {
-        return {0, 1, 2};
+        return {0, 1, 2, 3};
     }
 
-    void setUp(const celero::TestFixture::ExperimentValue&
-                   val) override
+    void
+    setUp(const celero::TestFixture::ExperimentValue& val) override
     {
-        const auto i = val.Value; 
-        // TODO: grr
-        state = std::make_unique<STUNState>(gsl::at(data, i)[0], gsl::at(data,i)[1]);
+        const auto i = val.Value;
+        state = std::make_unique<STUNState>(data[i][0], data[i][1]);
     }
 
+    [[nodiscard]] std::vector<
+        std::shared_ptr<celero::UserDefinedMeasurement>>
+    getUserDefinedMeasurements() const override
+    {
+        return {energies};
+    }
 
+    std::shared_ptr<LowestEnergyUDM> energies
+        = std::make_shared<LowestEnergyUDM>();
     std::unique_ptr<STUNState> state;
     std::array<std::array<gsl::index, 2>, 3> data{
-        {{5, 5}, {5, 10}, {10, 10}}};
+        {{5, 5}, {5, 10}, {10, 10}, {15, 15}}};
 };
 } // namespace
 
+// TODO: Measure different gamma & beta_scale
+
 // NOLINTNEXTLINE(readability-redundant-member-init)
-BASELINE_F(StochasticTunneling, Small, STUNFixture, 0, 0)
+BASELINE_F(StochasticTunneling, Small, STUNFixture, 5, 5)
 {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wmissing-braces"
     const auto result = stochastic_tunneling(
-        state.random_utils,
-        state.makespan,
-        state.best_state,
+        state->random_utils,
+        state->makespan,
+        state->best_state,
         Gamma{.5F},
         Beta{1.F},
         BetaScale{.3F});
     celero::DoNotOptimizeAway(result);
-    fmt::print("{}\n", result.energy);
-    for (const auto& v : result.state) fmt::print("{} ", v);
-    fmt::print("\n");
+    energies->addValue(result.energy);
+    // fmt::print("{}\n", result.energy);
+    // for (const auto& v : result.state) fmt::print("{} ", v);
+    // fmt::print("\n");
 #pragma clang diagnostic pop
 }
