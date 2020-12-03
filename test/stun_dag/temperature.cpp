c38cf05990df0a9a662e55fb43ef241f0c3638ca
Andrew <dronx.wow@gmail.com>
2020-12-03 03:22:26 +0300

    Add more Temperature tests

--- test/stun_dag/temperature.cpp
+++ test/stun_dag/temperature.cpp
@@ -11,3 +11,25 @@ TEST_CASE("Temperature type traits")
     STATIC_REQUIRE(std::is_move_constructible_v<Temperature>);
     STATIC_REQUIRE(std::is_move_assignable_v<Temperature>);
 }
+
+TEST_CASE("Temperature update")
+{
+    using namespace angonoka::stun_dag;
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-braces"
+    Temperature temp{Beta{1.f}, BetaScale{1e-3f}, StunWindow{5}};
+#pragma clang diagnostic pop
+
+    REQUIRE(temp == 1.f);
+
+    for (int i{0}; i < 10; ++i) temp.update(1.f, 0.f);
+
+    REQUIRE(temp.average_stun() == Approx(1.f));
+    REQUIRE(temp > 1.f);
+
+    for (int i{0}; i < 1000; ++i) temp.update(0.f, 0.f);
+
+    REQUIRE(temp.average_stun() == Approx(0.f).margin(0.001));
+    REQUIRE(temp < 1.f);
+}
