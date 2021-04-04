b55fbf52abe8f40748fd71df4c47b8cf9af031d4
Andrew <dronx.wow@gmail.com>
2021-04-05 02:20:10 +0300

    Add ProgressEstimator tests

--- test/stun/progress.cpp
+++ test/stun/progress.cpp
@@ -1,14 +1,31 @@
 #include <catch2/catch.hpp>
 
+#include <cmath>
+
 namespace angonoka::stun {
 // TODO: doc, test, expects
+// TODO: name? ExpCurveFitter?
 class ProgressEstimator {
 public:
     // TODO: doc, test, expects
-    void reset() noexcept;
+    void reset() noexcept { *this = ProgressEstimator(); }
 
     // TODO: doc, test, expects
-    float operator()(float x, float y) noexcept;
+    float operator()(float x, float y) noexcept
+    {
+        const auto xy1 = x * y;
+        const auto ylogy1 = y * std::log(y);
+        xxy += x * xy1;
+        ylogy += ylogy1;
+        xy += xy1;
+        xylogy += x * ylogy1;
+        sumy += y;
+        const auto divisor = sumy * xxy - std::pow(xy, 2.F);
+        if (divisor == 0.F) return 0.F;
+        const auto a = (xxy * ylogy - xy * xylogy) / divisor;
+        const auto b = (sumy * xylogy - xy * ylogy) / divisor;
+        return std::exp(b * x + a);
+    }
 
 private:
     float xxy = 0;
@@ -22,6 +39,20 @@ private:
 TEST_CASE("ProgressEstimator")
 {
     using angonoka::stun::ProgressEstimator;
-    ProgressEstimator p_estimator;
-    // TODO: tests
+    ProgressEstimator est;
+
+    REQUIRE(est(0.F, 0.F) == Approx(0.F));
+    est.reset();
+    est(1.F, .011108996538242F);
+    est(2.F, .018315638888734F);
+    est(3.F, .030197383422319F);
+    est(4.F, .049787068367864F);
+    REQUIRE(est(5.F, .082084998623899F) == Approx(.082084998623899F));
+    est(6.F, .135335283236613F);
+    est(7.F, .22313016014843F);
+    est(8.F, .367879441171442F);
+    est(9.F, .606530659712634F);
+    REQUIRE(est(10.F, 1.F) == Approx(1.F));
+    est.reset();
+    REQUIRE(est(0.F, 0.F) == Approx(0.F));
 }
