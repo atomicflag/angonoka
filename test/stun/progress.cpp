c0e781e541983eb66a9bd2bbf8e9a622e22fc4d8
Andrew <dronx.wow@gmail.com>
2021-04-05 21:44:56 +0300

    Update documentation

--- test/stun/progress.cpp
+++ test/stun/progress.cpp
@@ -1,18 +1,42 @@
 #include <catch2/catch.hpp>
 
 #include <cmath>
+#include <gsl/gsl-lite.hpp>
 
 namespace angonoka::stun {
-// TODO: doc, test, expects
-// TODO: name? ExpCurveFitter?
-class ProgressEstimator {
+/**
+    Online estimation of an exponential curve from noisy input.
+*/
+class ExpCurveFitter {
 public:
-    // TODO: doc, test, expects
-    void reset() noexcept { *this = ProgressEstimator(); }
+    /**
+        Reset the internal state.
+    */
+    void reset() noexcept
+    {
+        *this = ExpCurveFitter();
 
-    // TODO: doc, test, expects
+        Ensures(xxy == 0);
+        Ensures(ylogy == 0);
+        Ensures(xy == 0);
+        Ensures(xylogy == 0);
+        Ensures(sumy == 0);
+    }
+
+    /**
+        Update the estimate.
+
+        @param x Coordinate along the first axis
+        @param y Coordinate along the second axis
+
+        @return New estimate based on the latest data.
+    */
     float operator()(float x, float y) noexcept
     {
+        // Not strictly necessary
+        Expects(x >= 0.F);
+        Expects(y >= 0.F);
+
         const auto xy1 = x * y;
         const auto ylogy1 = y * std::log(y);
         xxy += x * xy1;
@@ -36,23 +60,25 @@ private:
 };
 } // namespace angonoka::stun
 
-TEST_CASE("ProgressEstimator")
-{
-    using angonoka::stun::ProgressEstimator;
-    ProgressEstimator est;
+// TODO: type traits tests
 
-    REQUIRE(est(0.F, 0.F) == Approx(0.F));
-    est.reset();
-    est(1.F, .011108996538242F);
-    est(2.F, .018315638888734F);
-    est(3.F, .030197383422319F);
-    est(4.F, .049787068367864F);
-    REQUIRE(est(5.F, .082084998623899F) == Approx(.082084998623899F));
-    est(6.F, .135335283236613F);
-    est(7.F, .22313016014843F);
-    est(8.F, .367879441171442F);
-    est(9.F, .606530659712634F);
-    REQUIRE(est(10.F, 1.F) == Approx(1.F));
-    est.reset();
-    REQUIRE(est(0.F, 0.F) == Approx(0.F));
+TEST_CASE("ExpCurveFitter basic operations")
+{
+    using angonoka::stun::ExpCurveFitter;
+    ExpCurveFitter fit;
+
+    REQUIRE(fit(0.F, 0.F) == Approx(0.F));
+    fit.reset();
+    fit(1.F, .011108996538242F);
+    fit(2.F, .018315638888734F);
+    fit(3.F, .030197383422319F);
+    fit(4.F, .049787068367864F);
+    REQUIRE(fit(5.F, .082084998623899F) == Approx(.082084998623899F));
+    fit(6.F, .135335283236613F);
+    fit(7.F, .22313016014843F);
+    fit(8.F, .367879441171442F);
+    fit(9.F, .606530659712634F);
+    REQUIRE(fit(10.F, 1.F) == Approx(1.F));
+    fit.reset();
+    REQUIRE(fit(0.F, 0.F) == Approx(0.F));
 }
