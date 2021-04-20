f5a42ab0895f5779ab1495145496d56df3f4ec40
Andrew <dronx.wow@gmail.com>
2021-04-20 03:47:52 +0300

    WIP: Added Optimizer stub [skip ci]

--- test/predict.cpp
+++ test/predict.cpp
@@ -1,10 +1,12 @@
-// TODO: remove this
-#undef UNIT_TEST
 #include <catch2/catch.hpp>
 
 #include "config/load.h"
 #include "configuration.h"
+#ifndef UNIT_TEST
 #include "stun/optimizer.h"
+#endif // UNIT_TEST
+#include "stun/common.h"
+#include "stun/schedule_params.h"
 #include <readerwriterqueue/readerwriterqueue.h>
 #include <future>
 #include <memory>
@@ -30,6 +32,21 @@ struct ScheduleOptimizationEvent {
 
 using ProgressEvent = boost::variant<SimpleProgressEvent, ScheduleOptimizationEvent>;
 
+#ifdef UNIT_TEST
+enum class BatchSize : std::int_fast32_t;
+enum class MaxIdleIters : std::int_fast32_t;
+struct OptimizerStub {
+    OptimizerStub(auto...) {}
+
+    bool has_converged() { return true; }
+    void update() {}
+    stun::State state() { return {}; }
+};
+using OptimizerT = OptimizerStub;
+#else
+using OptimizerT = Optimizer;
+#endif
+
 // TODO: doc, test, expects
 std::vector<stun::StateItem> optimize(
     const stun::ScheduleParams& params,
@@ -50,7 +67,7 @@ std::vector<stun::StateItem> optimize(
     // TODO: Dependency inversion?
     // Should this be a function object? Or 
     // should Optimizer be conditionally-mockable?
-    Optimizer optimizer{
+    OptimizerT optimizer{
         params,
         BatchSize{batch_size},
         MaxIdleIters{max_idle_iters}};
@@ -72,6 +89,7 @@ predict(const Configuration& config)
     auto events = std::make_shared<Queue<ProgressEvent>>(100);
     auto future = std::async(std::launch::async, [=] {
         events->enqueue(SimpleProgressEvent::Start);
+        // TODO: stub to_schedule, etc
         const auto schedule_params = stun::to_schedule_params(config);
         const auto state = optimize(schedule_params, *events);
         return Prediction{};
