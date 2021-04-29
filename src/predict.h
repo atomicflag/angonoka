1c0b53262b2f25acaad5b07b4d819d376620605e
Andrew <dronx.wow@gmail.com>
2021-04-30 00:11:46 +0300

    Add documentation

--- src/predict.h
+++ src/predict.h
@@ -15,23 +15,44 @@ struct Prediction {
 template <typename... Ts>
 using Queue = moodycamel::ReaderWriterQueue<Ts...>;
 
-// TODO: doc, test, expects
+/**
+    Events without attributes.
+*/
 enum class SimpleProgressEvent {
     ScheduleOptimizationStart,
     ScheduleOptimizationDone,
     Finished
 };
 
-// TODO: doc, test, expects
+/**
+    Emitted during the schedule optimization step.
+
+    @var progress Optimization progress from 0.0 to 1.0
+    @var makespan The best makespan so far in seconds
+*/
 struct ScheduleOptimizationEvent {
     float progress;
     float makespan;
 };
 
+/**
+    Prediction progress event.
+*/
 using ProgressEvent
     = boost::variant<SimpleProgressEvent, ScheduleOptimizationEvent>;
 
-// TODO: doc, test, expects
+/**
+    Predict likelihood of a given system configuration.
+
+    The event queue provides a way to monitor the progress
+    of the prediction function.
+
+    // TODO: test
+
+    @param config Tasks, agents and other data.
+
+    @return Prediction future and the event queue.
+*/
 std::tuple<
     std::future<Prediction>,
     std::shared_ptr<Queue<ProgressEvent>>>
