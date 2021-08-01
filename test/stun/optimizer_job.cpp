f9f22adb992f6c81072d1f9c7ed94a3351af0dc5
Andrew <dronx.wow@gmail.com>
2021-08-01 05:11:57 +0300

    Add more OptimizerJob tests

--- test/stun/optimizer_job.cpp
+++ test/stun/optimizer_job.cpp
@@ -15,7 +15,56 @@ suite optimizer_job = [] {
         expect(std::is_nothrow_move_assignable_v<OptimizerJob>);
     };
 
-    // TODO: Add OptimizerJob test
+    "basic OptimizerJob operations"_test = [] {
+        using namespace angonoka::stun;
+
+        // clang-format off
+        constexpr auto text = 
+            "agents:\n"
+            "  Bob:\n"
+            "  Jack:\n"
+            "tasks:\n"
+            "  - name: Task 1\n"
+            "    duration: 1h\n"
+            "  - name: Task 2\n"
+            "    duration: 1h";
+        // clang-format on
+        const auto config = angonoka::load_text(text);
+
+        const auto params = to_schedule_params(config);
+        OptimizerJob optimizer{params, BatchSize{5}};
+
+        expect(optimizer.normalized_makespan() == 2.F);
+        expect(optimizer.schedule()[1].agent_id == 0);
+
+        optimizer.update();
+
+        while (optimizer.normalized_makespan() != 1.F)
+            optimizer.update();
+
+        // Might be non-deterministic
+        expect(optimizer.normalized_makespan() == 1.F);
+        // Each task has a different agent
+        expect(
+            optimizer.schedule()[1].agent_id
+            != optimizer.schedule()[0].agent_id);
+
+        should("reset") = [&] {
+            optimizer.reset();
+
+            expect(optimizer.normalized_makespan() == 2.F);
+        };
+
+        should("rebind params") = [&] {
+            expect(&optimizer.params() == &params);
+            const auto params2 = params;
+            optimizer.params(params2);
+            expect(&optimizer.params() == &params2);
+
+            while (optimizer.normalized_makespan() != 1.F)
+                optimizer.update();
+        };
+    };
 
     "OptimizerJob special memeber functions"_test = [] {
         using namespace angonoka::stun;
