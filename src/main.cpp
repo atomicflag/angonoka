6dc8adf3663c89103cf39bc6fa8a646cb070a02b
Andrew <dronx.wow@gmail.com>
2021-08-28 05:29:06 +0300

    Update CLI [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -12,23 +12,35 @@ int main(int argc, char** argv)
     register_abort_handlers();
 
     Options options;
-    CLI::App app{
+    CLI::App cli{
         "Angonoka is a time estimation software based on statistical "
         "modeling.\n",
         ANGONOKA_NAME};
+    // auto* app = cli.add_subcommand();
+    auto* app = &cli;
 
-    auto* version = app.add_flag(
+    auto* version = app->add_flag(
         "--version",
         "Display program version information and exit");
 
-    app.add_flag(
+    app->add_flag(
         "--color,!--no-color",
         options.color,
         "Force colored output");
-    app.add_flag("-q,--quiet", options.quiet, "Give less output");
-    app.add_flag("-v,--verbose", options.verbose, "Give more output");
-    app.add_option("input file", options.filename)->required();
+    app->add_flag("-q,--quiet", options.quiet, "Give less output");
+    app->add_flag("-v,--verbose", options.verbose, "Give more output");
 
+    auto* input_file = app->add_option_group("input");
+    input_file->add_option("input file", options.filename)->required()->check(CLI::ExistingFile);
+
+    auto* schedule_cmd = cli.add_subcommand("schedule", "Output the schedule in JSON format.");
+    schedule_cmd->fallthrough();
+    // schedule_cmd->needs(input_file);
+    CLI::TriggerOff(schedule_cmd, input_file);
+    schedule_cmd->add_option("input file", options.filename)->required()->check(CLI::ExistingFile);
+    // TODO: CLI help doesn't look clean
+
+    // TODO: CLI11 supports --version now
     const auto version_requested = [&] {
         if (version->count() > 0) {
             fmt::print("{}\n", ANGONOKA_VERSION);
@@ -38,14 +50,14 @@ int main(int argc, char** argv)
     };
 
     try {
-        app.parse(argc, argv);
+        cli.parse(argc, argv);
         if (version_requested()) return EXIT_SUCCESS;
         const auto config = parse_config(options);
         run_prediction(config, options);
         return EXIT_SUCCESS;
     } catch (const CLI::ParseError& e) {
         if (version_requested()) return EXIT_SUCCESS;
-        return app.exit(e);
+        return cli.exit(e);
     } catch (const UserError&) {
         return EXIT_FAILURE;
     }
