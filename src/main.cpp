e5728820641ee7d23c6aa083bede2c010cf7cf10
Andrew <dronx.wow@gmail.com>
2021-08-29 06:03:14 +0300

    Add builtin CLI version option [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -16,47 +16,32 @@ int main(int argc, char** argv)
         "Angonoka is a time estimation software based on statistical "
         "modeling.\n",
         ANGONOKA_NAME};
-    // auto* app = cli.add_subcommand();
-    auto* app = &cli;
 
-    auto* version = app->add_flag(
-        "--version",
-        "Display program version information and exit");
+    cli.set_version_flag("--version", ANGONOKA_VERSION,"Display program version information and exit" );
 
-    app->add_flag(
+    cli.add_flag(
         "--color,!--no-color",
         options.color,
         "Force colored output");
-    app->add_flag("-q,--quiet", options.quiet, "Give less output");
-    app->add_flag("-v,--verbose", options.verbose, "Give more output");
+    cli.add_flag("-q,--quiet", options.quiet, "Give less output");
+    cli.add_flag("-v,--verbose", options.verbose, "Give more output");
 
-    auto* input_file = app->add_option_group("input");
-    input_file->add_option("input file", options.filename)->required()->check(CLI::ExistingFile);
+    auto* subcommands = cli.add_subcommand("");
+    // auto* subcommands = cli.add_option_group("General");
+    subcommands->add_option("input file", options.filename)->required()->check(CLI::ExistingFile);
 
-    auto* schedule_cmd = cli.add_subcommand("schedule", "Output the schedule in JSON format.");
+    auto* schedule_cmd = subcommands->add_subcommand("schedule", "Output the schedule in JSON format.");
     schedule_cmd->fallthrough();
-    // schedule_cmd->needs(input_file);
-    CLI::TriggerOff(schedule_cmd, input_file);
-    schedule_cmd->add_option("input file", options.filename)->required()->check(CLI::ExistingFile);
+    // CLI::TriggerOff(schedule_cmd, input_file);
+    // schedule_cmd->add_option("input file", options.filename)->required()->check(CLI::ExistingFile);
     // TODO: CLI help doesn't look clean
-
-    // TODO: CLI11 supports --version now
-    const auto version_requested = [&] {
-        if (version->count() > 0) {
-            fmt::print("{}\n", ANGONOKA_VERSION);
-            return true;
-        }
-        return false;
-    };
 
     try {
         cli.parse(argc, argv);
-        if (version_requested()) return EXIT_SUCCESS;
         const auto config = parse_config(options);
         run_prediction(config, options);
         return EXIT_SUCCESS;
     } catch (const CLI::ParseError& e) {
-        if (version_requested()) return EXIT_SUCCESS;
         return cli.exit(e);
     } catch (const UserError&) {
         return EXIT_FAILURE;
