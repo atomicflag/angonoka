3817ae302b41c600b81eaca6c87dd5090cffd5a4
Andrew <dronx.wow@gmail.com>
2021-09-19 04:31:01 +0300

    Update schedule to JSON tests

--- test/cli/json_schedule.cpp
+++ test/cli/json_schedule.cpp
@@ -6,7 +6,7 @@ using namespace boost::ut;
 
 suite json_schedule = [] {
     using namespace angonoka;
-    "basic operation"_test = [] {
+    "basic schedule"_test = [] {
         // clang-format off
         constexpr auto text =
             "agents:\n"
@@ -32,7 +32,7 @@ suite json_schedule = [] {
         expect(task["expected_start"] == 0_i);
     };
 
-    "2 tasks, 2 agents"_test = [] {
+    "advanced schedule"_test = [] {
         // clang-format off
         constexpr auto text =
             "agents:\n"
@@ -41,8 +41,18 @@ suite json_schedule = [] {
             "tasks:\n"
             "  - name: task 1\n"
             "    duration: 1h\n"
+            "    id: A\n"
             "  - name: task 2\n"
-            "    duration: 1h";
+            "    duration: 1h\n"
+            "    depends_on: A\n"
+            "    id: B\n"
+            "  - name: task 3\n"
+            "    duration: 1h\n"
+            "    depends_on: B\n"
+            "    id: C\n"
+            "  - name: task 4\n"
+            "    duration: 1h\n"
+            "    depends_on: C";
         // clang-format on
         const auto config = load_text(text);
 
@@ -50,7 +60,9 @@ suite json_schedule = [] {
             .makespan{3600},
             .schedule{
                 {.task_id{0}, .agent_id{0}},
-                {.task_id{1}, .agent_id{1}}}};
+                {.task_id{1}, .agent_id{1}},
+                {.task_id{2}, .agent_id{1}},
+                {.task_id{3}, .agent_id{0}}}};
         const auto json = cli::detail::to_json(config, schedule);
 
         expect(
@@ -67,7 +79,17 @@ suite json_schedule = [] {
                    {"agent", "agent2"},
                    {"priority", 0},
                    {"expected_duration", 3600},
-                   {"expected_start", 0}}}}});
+                   {"expected_start", 3600}},
+                  {{"task", "task 3"},
+                   {"agent", "agent2"},
+                   {"priority", 1},
+                   {"expected_duration", 3600},
+                   {"expected_start", 7200}},
+                  {{"task", "task 4"},
+                   {"agent", "agent1"},
+                   {"priority", 1},
+                   {"expected_duration", 3600},
+                   {"expected_start", 10800}}}}});
     };
 
     // TODO: WIP: More complex tests
