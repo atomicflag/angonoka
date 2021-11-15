4a4355ce2310218d022aec9697d48b28be8b41e0
Andrew <dronx.wow@gmail.com>
2021-11-16 00:13:27 +0300

    Add Button test

---
+++ tools/visualizer/src/components/Button.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import Button from "./Button";
+
+test("Button creation", () => {
+  const { queryByText } = render(<Button text="hello" />);
+
+  const button = queryByText("hello");
+  expect(button.nodeName).toEqual("BUTTON");
+  expect(queryByText("hello")).toBeEnabled();
+});
