86ab9ca306e1a1ce0a58929b7ec2430336f022a4
Andrew <dronx.wow@gmail.com>
2021-11-19 05:19:57 +0300

    Add schedule loading

--- tools/visualizer/src/components/App.tsx
+++ tools/visualizer/src/components/App.tsx
@@ -6,12 +6,52 @@ type State = {
   schedule: any;
 };
 
+const schedule = `
+{
+  "makespan": 720,
+  "tasks": [
+    {
+      "agent": "Agent 3",
+      "expected_duration": 180,
+      "expected_start": 0.0,
+      "priority": 0,
+      "task": "Task 1"
+    },
+    {
+      "agent": "Agent 2",
+      "expected_duration": 600,
+      "expected_start": 0.0,
+      "priority": 0,
+      "task": "Task 2"
+    },
+    {
+      "agent": "Agent 1",
+      "expected_duration": 330,
+      "expected_start": 0.0,
+      "priority": 0,
+      "task": "Task 3"
+    },
+    {
+      "agent": "Agent 1",
+      "expected_duration": 390,
+      "expected_start": 330.0,
+      "priority": 1,
+      "task": "Task 4"
+    }
+  ]
+}
+`;
+
 export default class App extends React.Component<{}, State> {
   fileUpload: React.RefObject<HTMLInputElement>;
 
   constructor(props: {}) {
     super(props);
     this.fileUpload = React.createRef();
+    // Temporary hardcode the schedule so that
+    // we don't have to load it each time.
+    // TODO: Remove this
+    this.state = { schedule: JSON.parse(schedule) };
   }
 
   render() {
@@ -22,15 +62,24 @@ export default class App extends React.Component<{}, State> {
           <Button
             text="Load"
             className="ml-2"
-            onClick={this.loadSchedule.bind(this)}
+            onClick={() => this.fileUpload.current.click()}
           />
-          <input type="file" ref={this.fileUpload} className="hidden" />
+          <input
+            type="file"
+            ref={this.fileUpload}
+            className="hidden"
+            onChange={this.loadSchedule.bind(this)}
+            accept=".json"
+          />
         </div>
       </div>
     );
   }
 
-  private loadSchedule() {
-    this.fileUpload.current.click();
+  private async loadSchedule() {
+    const fu = this.fileUpload.current;
+    if (fu.files.length === 0) return;
+    const text = await fu.files[0].text();
+    this.setState({ schedule: JSON.parse(text) });
   }
 }
