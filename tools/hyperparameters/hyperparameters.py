9d38a1f865d70aef7282ce09abe4a020dbe858ec
Andrew <dronx.wow@gmail.com>
2022-01-18 01:13:49 +0300

    Update hyperparameter optimization script

--- tools/hyperparameters/hyperparameters.py
+++ tools/hyperparameters/hyperparameters.py
@@ -2,29 +2,46 @@ import logging
 import sys
 import optuna
 import subprocess
+from pathlib import Path
+from itertools import chain
 
 EXE = "../../build/src/angonoka-x86_64"
 
 optuna.logging.get_logger("optuna").addHandler(logging.StreamHandler(sys.stdout))
 study_name = "angonoka-hyperparameters"
 storage_name = f"sqlite:///{study_name}.db"
-study = optuna.create_study(study_name=study_name, storage=storage_name, load_if_exists=True)
+study = optuna.create_study(
+    study_name=study_name, storage=storage_name, load_if_exists=True
+)
 
-def run(*args):
-    r = subprocess.run(
-        [EXE, "--log-optimization-progress", *args])
-    # TODO: parse return code
-    # TODO: parse optimization.csv, find makespan and num iters
+
+def parameters(trial):
+    batch_size = trial.suggest_int("batch_size", 10, 500000)
+    params = [
+        ["--batch-size", batch_size],
+        ["--max-idle-iters", batch_size * trial.suggest_int("max_idle_iters", 1, 150)],
+        ["--beta-scale", trial.suggest_float("beta_scale", 1e-7, 1)],
+        ["--stun-window", trial.suggest_int("stun_window", 10, 500000)],
+        ["--gamma", trial.suggest_float("gamma", 0.01, 0.99)],
+        ["--restart-period", 2 ** trial.suggest_int("restart-period", 5, 28)],
+    ]
+    return [str(a) for a in chain(*params)]
+
+
+def run(args):
+    r = subprocess.run([EXE, *args, "--log-optimization-progress", "tasks.yml"])
+    assert r.returncode == 0
+    log = Path("optimization_log.csv").read_text().splitlines()[-1]
+    makespan, epoch = [int(a) for a in log.split(",")[1:3]]
+    return makespan
 
 
 def objective(trial):
-    # TODO: run "run" with correct CLI parameters
-    x = trial.suggest_float("x", -10, 10)
-    # TODO: figure out how to combine makespan and num iters
-    return (x - 2) ** 2
+    params = parameters(trial)
+    return run(params)
 
 
-study.optimize(objective, n_trials=3)
+study.optimize(objective)
 print("Best params: ", study.best_params)
 print("Best value: ", study.best_value)
 print("Best Trial: ", study.best_trial)
