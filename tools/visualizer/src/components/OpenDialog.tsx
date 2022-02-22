b83480a21a6e6775f00ca67f5e453d5759ed30d5
Andrew <dronx.wow@gmail.com>
2022-02-22 04:38:44 +0300

    Refactor Dialog component

--- tools/visualizer/src/components/OpenDialog.tsx
+++ tools/visualizer/src/components/OpenDialog.tsx
@@ -19,41 +19,68 @@ function Opener(
   };
 }
 
-// TODO: refactor
-function dialog(
-  isVisible: boolean,
-  setIsVisible: Dispatch<boolean>,
-  onOpen: (schedule: Schedule) => void
-) {
-  if (!isVisible) return;
+type ClosableProps = {
+  setIsVisible: Dispatch<boolean>;
+};
+
+function Background({ setIsVisible }: ClosableProps) {
+  return (
+    <div className={style.background} onClick={() => setIsVisible(false)}>
+      &nbsp;
+    </div>
+  );
+}
+
+function CloseButton({ setIsVisible }: ClosableProps) {
+  return (
+    <a
+      href="#"
+      className={style.closeButton}
+      onClick={() => setIsVisible(false)}
+    >
+      ×
+    </a>
+  );
+}
+
+type OpenerProps = {
+  opener: (schedule: Schedule) => void;
+};
+
+function Layout({ opener }: OpenerProps) {
+  return (
+    <div className={style.layout}>
+      <div className={style.upload}>
+        <span className="mb-4">Upload an optimized schedule JSON</span>
+        <ScheduleUpload onUpload={opener} />
+      </div>
+      <div className={style.separator}>─── or ───</div>
+      <div className={style.clipboard}>
+        <span className="mb-4 text-center">
+          paste the schedule from clipboard
+        </span>
+        <SchedulePaste onPaste={opener} />
+      </div>
+    </div>
+  );
+}
+
+type DialogProps = {
+  isVisible: boolean;
+  setIsVisible: Dispatch<boolean>;
+  onOpen: (schedule: Schedule) => void;
+};
+
+function Dialog({ isVisible, setIsVisible, onOpen }: DialogProps) {
+  if (!isVisible) return null;
   const opener = Opener(setIsVisible, onOpen);
   return (
     <div className={style.openDialog}>
-      <div className={style.background} onClick={() => setIsVisible(false)}>
-        &nbsp;
-      </div>
+      <Background setIsVisible={setIsVisible} />
       <div className={style.windowContainer}>
         <div className={style.window}>
-          <a
-            href="#"
-            className={style.closeButton}
-            onClick={() => setIsVisible(false)}
-          >
-            ×
-          </a>
-          <div className={style.layout}>
-            <div className={style.upload}>
-              <span className="mb-4">Upload an optimized schedule JSON</span>
-              <ScheduleUpload onUpload={opener} />
-            </div>
-            <div className={style.separator}>─── or ───</div>
-            <div className={style.clipboard}>
-              <span className="mb-4 text-center">
-                paste the schedule from clipboard
-              </span>
-              <SchedulePaste onPaste={opener} />
-            </div>
-          </div>
+          <CloseButton setIsVisible={setIsVisible} />
+          <Layout opener={opener} />
         </div>
       </div>
     </div>
@@ -65,7 +92,11 @@ export const OpenDialog = ({ onOpen }: Props) => {
   return (
     <div>
       <Button text="Open" className="ml-2" onClick={() => setIsVisible(true)} />
-      {dialog(isVisible, setIsVisible, onOpen)}
+      <Dialog
+        isVisible={isVisible}
+        setIsVisible={setIsVisible}
+        onOpen={onOpen}
+      />
     </div>
   );
 };
