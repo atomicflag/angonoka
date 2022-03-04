cb415b849eee5dfb4066907f314ca0340413ce33
Andrew <dronx.wow@gmail.com>
2022-03-05 02:30:32 +0300

    WIP: Add Simulation buffer assignment [skip ci]

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,16 +1,38 @@
 #include "simulation.h"
 
-namespace angonoka {
-namespace detail {
-    Simulation::Simulation(
-            const Configuration& config,
-            const OptimizedSchedule& schedule,
-            stun::RandomUtils& random) : config{&config}, schedule{&schedule}, random{&random} {
+namespace angonoka::detail {
+Simulation::Simulation(
+    const Configuration& config,
+    const OptimizedSchedule& schedule,
+    stun::RandomUtils& random)
+    : config{&config}
+    , schedule{&schedule}
+    , random{&random}
+    , buffer(config.agents.size() * 2 + config.tasks.size() * 2)
+{
+    Expects(
+        buffer.size()
+        == config.agents.size() * 2 + config.tasks.size() * 2);
 
-    }
+    const auto agent_count = std::ssize(config.agents);
+    const auto task_count = std::ssize(config.tasks);
+    auto* head = buffer.data();
+    auto assign_span = [&](auto& span, auto count) {
+        span = {std::exchange(head, std::next(head, count)), count};
+    };
+    assign_span(agent_performance, agent_count);
+    assign_span(task_duration, task_count);
+    assign_span(agent_work_end, agent_count);
+    assign_span(task_done, task_count);
 
-        [[nodiscard]] std::chrono::seconds Simulation::operator()() noexcept {
-            return {};
-        }
+    Ensures(agent_performance.size() == std::ssize(config.agents));
+    Ensures(task_duration.size() == std::ssize(config.tasks));
+    Ensures(agent_work_end.size() == std::ssize(config.agents));
+    Ensures(task_done.size() == std::ssize(config.tasks));
 }
+
+[[nodiscard]] std::chrono::seconds Simulation::operator()() noexcept
+{
+    return {};
 }
+} // namespace angonoka::detail
--- src/simulation.h
+++ src/simulation.h
@@ -8,32 +8,33 @@
 #include <range/v3/view/span.hpp>
 #include <vector>
 
-namespace angonoka {
-namespace detail {
+namespace angonoka::detail {
+// TODO: doc, test, expects
+class Simulation {
+public:
     // TODO: doc, test, expects
-    class Simulation {
-    public:
-        // TODO: doc, test, expects
-        Simulation(
-            const Configuration& config,
-            const OptimizedSchedule& schedule,
-            stun::RandomUtils& random);
+    Simulation(
+        const Configuration& config,
+        const OptimizedSchedule& schedule,
+        stun::RandomUtils& random);
 
-        // TODO: doc, test, expects
-        [[nodiscard]] std::chrono::seconds operator()() noexcept;
+    // TODO: doc, test, expects
+    [[nodiscard]] std::chrono::seconds operator()() noexcept;
 
-    private:
-        struct Impl;
-        gsl::not_null<const Configuration*> config;
-        gsl::not_null<const OptimizedSchedule*> schedule;
-        gsl::not_null<stun::RandomUtils*> random;
-        std::vector<float> buffer;
-        ranges::span<float> agent_performance;
-        ranges::span<float> task_duration;
-        ranges::span<float> agent_work_end;
-        ranges::span<float> task_done;
-    };
-} // namespace detail
+private:
+    struct Impl;
+    gsl::not_null<const Configuration*> config;
+    gsl::not_null<const OptimizedSchedule*> schedule;
+    gsl::not_null<stun::RandomUtils*> random;
+    std::vector<float> buffer;
+    ranges::span<float> agent_performance;
+    ranges::span<float> task_duration;
+    ranges::span<float> agent_work_end;
+    ranges::span<float> task_done;
+};
+} // namespace angonoka::detail
+
+namespace angonoka {
 // boost::histogram histogram(
 //   const Configuration& config,
 //   const OptimizedSchedule& schedule);
