7e740380a7c0b456bab8b2bc18cf12e73244e968
Andrew <dronx.wow@gmail.com>
2022-03-07 01:32:05 +0300

    WIP: Add random agent performances and task durations [skip ci]

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -6,40 +6,71 @@ struct Simulation::Impl {
     // TODO: doc, test, expects
     static void assign_buffers(Simulation& self)
     {
-        Expects(!self.config->agents.empty());
-        Expects(!self.config->tasks.empty());
+        using ranges::span;
+        const auto& agents = self.config->agents;
+        const auto& tasks = self.config->tasks;
 
-        self.buffer.resize(
-            self.config->agents.size() * 2
-            + self.config->tasks.size() * 2);
-        const auto agent_count = std::ssize(self.config->agents);
-        const auto task_count = std::ssize(self.config->tasks);
+        Expects(!agents.empty());
+        Expects(!tasks.empty());
+
+        self.buffer.resize(agents.size() * 2 + tasks.size() * 2);
+        const auto agent_count = std::ssize(agents);
+        const auto task_count = std::ssize(tasks);
         auto* head = self.buffer.data();
-        auto assign_span = [&](auto& span, auto count) {
-            span
-                = {std::exchange(head, std::next(head, count)),
-                   count};
+        const auto next_span = [&](auto count) {
+            return span{
+                std::exchange(head, std::next(head, count)),
+                count};
         };
-        assign_span(self.agent_performance, agent_count);
-        assign_span(self.task_duration, task_count);
-        assign_span(self.agent_work_end, agent_count);
-        assign_span(self.task_done, task_count);
+        self.agent_performance = next_span(agent_count);
+        self.task_duration = next_span(task_count);
+        self.agent_work_end = next_span(agent_count);
+        self.task_done = next_span(task_count);
 
         Ensures(
             self.buffer.size()
-            == self.config->agents.size() * 2
-                + self.config->tasks.size() * 2);
-        Ensures(
-            self.agent_performance.size()
-            == std::ssize(self.config->agents));
-        Ensures(
-            self.task_duration.size()
-            == std::ssize(self.config->tasks));
-        Ensures(
-            self.agent_work_end.size()
-            == std::ssize(self.config->agents));
-        Ensures(
-            self.task_done.size() == std::ssize(self.config->tasks));
+            == agents.size() * 2 + tasks.size() * 2);
+        Ensures(self.agent_performance.data() == self.buffer.data());
+        Ensures(self.agent_performance.size() == std::ssize(agents));
+        Ensures(self.task_duration.size() == std::ssize(tasks));
+        Ensures(self.agent_work_end.size() == std::ssize(agents));
+        Ensures(self.task_done.size() == std::ssize(tasks));
+    }
+
+    // TODO: doc, test, expects
+    static void random_agent_performances(Simulation& self)
+    {
+        using index = decltype(self.agent_performance)::index_type;
+        for (index i = 0; auto&& agent : self.config->agents) {
+            float perf = self.random->normal(
+                agent.performance.min,
+                agent.performance.max);
+            while (perf <= 0.F) {
+                perf = self.random->normal(
+                    agent.performance.min,
+                    agent.performance.max);
+            }
+            self.agent_performance[i] = perf;
+            ++i;
+        }
+    }
+
+    // TODO: doc, test, expects
+    static void random_task_durations(Simulation& self)
+    {
+        using index = decltype(self.task_duration)::index_type;
+        for (index i = 0; auto&& task : self.config->tasks) {
+            const auto min
+                = static_cast<float>(task.duration.min.count());
+            const auto max
+                = static_cast<float>(task.duration.max.count());
+            float duration = self.random->normal(min, max);
+            while (duration <= 0.F) {
+                duration = self.random->normal(min, max);
+            }
+            self.task_duration[i] = duration;
+            ++i;
+        }
     }
 };
 
@@ -56,6 +87,7 @@ Simulation::Simulation(const Params& params)
     Ensures(
         buffer.size()
         == config->agents.size() * 2 + config->tasks.size() * 2);
+    Ensures(agent_performance.data() == buffer.data());
     Ensures(agent_performance.size() == std::ssize(config->agents));
     Ensures(task_duration.size() == std::ssize(config->tasks));
     Ensures(agent_work_end.size() == std::ssize(config->agents));
@@ -80,6 +112,7 @@ void Simulation::params(const Params& params)
     Ensures(
         buffer.size()
         == config->agents.size() * 2 + config->tasks.size() * 2);
+    Ensures(agent_performance.data() == buffer.data());
     Ensures(agent_performance.size() == std::ssize(config->agents));
     Ensures(task_duration.size() == std::ssize(config->tasks));
     Ensures(agent_work_end.size() == std::ssize(config->agents));
@@ -88,6 +121,8 @@ void Simulation::params(const Params& params)
 
 [[nodiscard]] std::chrono::seconds Simulation::operator()() noexcept
 {
+    Impl::random_agent_performances(*this);
+    Impl::random_task_durations(*this);
     return {};
 }
 } // namespace angonoka::detail
