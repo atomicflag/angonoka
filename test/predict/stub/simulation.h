19f6ad7313c2770fa7707e4d5d7148e6611d109c
Andrew <dronx.wow@gmail.com>
2022-03-29 03:14:58 +0300

    WIP: Remove extra comments [skip ci]

--- test/predict/stub/simulation.h
+++ test/predict/stub/simulation.h
@@ -6,16 +6,5 @@ namespace angonoka{
 [[nodiscard]] Histogram histogram(
     const Configuration& config,
     const OptimizedSchedule& schedule);
-
-/**
-    Find various percentiles of the histogram.
-
-    Find the makespan for 25, 50, 75, 95 and 99 percent
-    confidence intervals.
-
-    @param histogram Makespan histogram
-
-    @return A struct of percentile values.
-*/
 HistogramStats stats(const Histogram& histogram);
 } // namespace angonoka
