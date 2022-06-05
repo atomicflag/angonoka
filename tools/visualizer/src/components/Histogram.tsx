6cdf4566c7b682c9c7a15141c270d3d7d8b073ba
Andrew <dronx.wow@gmail.com>
2022-06-05 19:57:32 +0300

    WIP: Add histogram bars [skip ci]

--- tools/visualizer/src/components/Histogram.tsx
+++ tools/visualizer/src/components/Histogram.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./Histogram.module.css";
 import { Histogram } from "../types";
+import lodash from "lodash";
 
 type Props = {
   className?: string;
@@ -8,10 +9,20 @@ type Props = {
 };
 
 function bucket(height: number) {
-  return <div>{height}</div>;
+  return <div className={style.bucket}>{height}</div>;
 }
 
 function buckets(histogram: Histogram) {
+  const max = lodash.chain(histogram.buckets).flatMap("[1]").max().value();
+  const buckets = new Map(histogram.buckets);
+  const start = histogram.buckets[0][0];
+  const end = lodash.last(histogram.buckets)[0];
+  let items = [];
+  // TODO: use lodash.range instead
+  // TODO: use i as react's key for items
+  for(let i = start; i <= end; i += histogram.bucket_size) {
+  console.info(i);
+  }
   return histogram.buckets.map(b=>bucket(b[1]));
 }
 
