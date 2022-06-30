aa266e8458d66835769ce7d0a83061df7209e74d
Andrew <dronx.wow@gmail.com>
2022-06-30 22:45:30 +0300

    WIP: Fix Histogram iterators [skip ci]

--- test/histogram.cpp
+++ test/histogram.cpp
@@ -1,7 +1,6 @@
 #include <catch2/catch.hpp>
 
 #include <boost/container/flat_map.hpp>
-#include <boost/iterator/transform_iterator.hpp>
 #include <boost/safe_numerics/safe_integer.hpp>
 #include <cstdint>
 #include <range/v3/range/concepts.hpp>
@@ -17,33 +16,101 @@ struct Bucket {
     operator int32() const { return count; }
 };
 
-using HistogramData = boost::container::flat_map<int32, int32>;
-
-// TODO: doc, test, expects
-struct BucketIter {
-    Bucket operator()(const auto& v) const
-    {
-        return {
-            .count{v.second},
-            .low{v.first},
-            .middle{v.first + bucket_size / 2},
-            .high{v.first + bucket_size}};
-    }
-
-    int32 bucket_size;
-};
-
-// class BucketIt : public boost::transform_iterator<BucketIter, HistogramData::iterator> {
-//     public:
-//         using transform_iterator::transform_iterator;
-//         using iterator_category = std::random_access_iterator_tag;
-//         // TODO: Wrong iterator category
-//         using transform_iterator::operator++;
-// };
 
 // TODO: doc, test, expects
 class Histogram {
 public:
+    using Data = boost::container::flat_map<int32, int32>;
+    // TODO: doc, test, expects
+    class Iterator {
+        public:
+            using difference_type = Data::const_iterator::difference_type;
+            using value_type = Bucket;
+
+            // TODO: doc, test, expects
+            Iterator(Data::const_iterator iter, int32 bucket_size) noexcept : iter{iter}, bucket_size{bucket_size} {}
+            Iterator() noexcept = default;
+
+            Bucket operator*() const {
+                return to_bucket(*iter);
+            }
+
+            friend bool operator==(const Iterator& b, const Iterator& a) noexcept {
+                return a.iter == b.iter;
+            }
+
+            Iterator& operator++() noexcept {
+                ++iter;
+                return *this;
+            }
+
+            Iterator operator++(int) noexcept {
+                return {iter++, bucket_size};
+            }
+
+            Iterator& operator--() noexcept {
+                --iter;
+                return *this;
+            }
+
+            Iterator operator--(int) noexcept {
+                return {iter--, bucket_size};
+            }
+
+            friend difference_type operator-(const Iterator& a, const Iterator& b) noexcept {
+                return a.iter - b.iter;
+            }
+
+            friend Iterator operator+(const Iterator& it, difference_type i) noexcept {
+                return {it.iter+i, it.bucket_size};
+            }
+
+            friend Iterator operator+(difference_type i, const Iterator& it) noexcept {
+                return {it.iter+i, it.bucket_size};
+            }
+
+            friend Iterator operator-(const Iterator& it, difference_type i) noexcept {
+                return {it.iter-i, it.bucket_size};
+            }
+
+            friend Iterator operator-(difference_type i, const Iterator& it) noexcept {
+                return {it.iter-i, it.bucket_size};
+            }
+
+            Iterator& operator+=(difference_type i) noexcept {
+                iter += i;
+                return *this;
+            }
+
+            Iterator& operator-=(difference_type i) noexcept {
+                iter -= i;
+                return *this;
+            }
+
+            Bucket operator[](difference_type i) const noexcept {
+                return to_bucket(iter[i]);
+            }
+
+            std::strong_ordering operator<=>(const Iterator& other) const noexcept {
+                if(iter == other.iter) return std::strong_ordering::equal;
+                if(iter < other.iter) return std::strong_ordering::less;
+                return std::strong_ordering::greater;
+            }
+
+        private:
+                Data::const_iterator iter;
+                int32 bucket_size;
+
+                Bucket to_bucket(const auto& v) const {
+            return {
+                .count{v.second},
+                .low{v.first},
+                .middle{v.first + bucket_size / 2},
+                .high{v.first + bucket_size}};
+                }
+
+    };
+
     // TODO: doc, test, expects
     Histogram(int32 bucket_size)
         : bucket_size{bucket_size}
@@ -61,58 +128,29 @@ public:
     void clear() { buckets.clear(); }
 
     // TODO: doc, test, expects
-    auto begin() noexcept
+    Iterator begin() const noexcept
     {
-        return boost::make_transform_iterator(            
-                buckets.begin(),
-BucketIter{bucket_size}
-                );
+        return Iterator{buckets.begin(), bucket_size};
     }
 
     // TODO: doc, test, expects
-    auto end() noexcept
+    Iterator end() const noexcept
     {
-        return boost::make_transform_iterator(            
-                buckets.end(),
-BucketIter{bucket_size}
-                );
+        return Iterator{buckets.end(), bucket_size};
     }
 
     auto operator[](int32 index) const {
-        return BucketIter{bucket_size}(*buckets.find(index));
+        return *Iterator{buckets.find(index), bucket_size};
     }
 
     auto size() const { return buckets.size(); }
 
 private:
     int32 bucket_size;
-    HistogramData buckets;
+    Data buckets;
 };
 } // namespace angonoka::detail
 
-namespace  {
-
-using HistogramIt = decltype(std::declval<angonoka::detail::Histogram>().begin());
-} // namespace 
-
-namespace std {
-    template<>
-    struct iterator_traits<HistogramIt> {
-        using difference_type = iterator_traits<angonoka::detail::HistogramData::iterator>::difference_type;
-        using value_type = std::indirectly_readable_traits<HistogramIt>::value_type;
-        // using value_type = angonoka::detail::Bucket;
-        using pointer = void;
-        using reference = decltype(std::declval<HistogramIt&>()[0]);
-        using iterator_category = std::random_access_iterator_tag;
-        using iterator_concept = std::random_access_iterator_tag;
-    };
-} // namespace std
-
-namespace ranges {
-    template<>
-    constexpr bool enable_borrowed_range<angonoka::detail::Histogram> = true;
-    // constexpr bool random_access_range<angonoka::detail::Histogram> = true;
-} // namespace ranges
 
 TEST_CASE("histogram concepts")
 {
@@ -123,21 +161,20 @@ TEST_CASE("histogram concepts")
 
     Histogram hist{42};
     ranges::begin(hist);
-    // STATIC_REQUIRE(ranges::detail::_borrowed_range<Histogram>);
-    // STATIC_REQUIRE(ranges::input_or_output_iterator<decltype(hist.begin())>);
-    // STATIC_REQUIRE(ranges::_begin_::has_member_begin<Histogram>);
-    // STATIC_REQUIRE(ranges::range<Histogram>);
-    // STATIC_REQUIRE(ranges::sized_range<Histogram>);
+    STATIC_REQUIRE(ranges::sized_range<Histogram>);
     STATIC_REQUIRE(ranges::random_access_range<Histogram>);
+    ranges::end(hist);
     STATIC_REQUIRE(requires(Histogram h) {
         {
             *h.begin()
             } -> std::same_as<Bucket>;
     });
+    // TODO: fix tests
     for(const auto& v : hist) {}
     hist.begin() == hist.end();
     auto i = hist.begin();
-    ranges::iter_reference_t<decltype(i)>::foo = 1; // >:(
+    i = hist.end();
+    // ranges::iter_reference_t<decltype(i)>::foo = 1; // >:(
     // decltype(i[5])::foo = 1;
-    STATIC_REQUIRE(std::same_as<decltype(i)&, decltype(++i)>);
+    // STATIC_REQUIRE(std::same_as<decltype(i)&, decltype(++i)>);
 }
